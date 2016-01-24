37a957b34366f0734cdcc604ef01489d28e80dd9
Ismail Mustafa <ismailmustafa@rocketmail.com>
2016-01-24 10:05:17 -0500

    First version of API complete

--- src/Api.hs
+++ src/Api.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Api
-    ( startApp
+    ( startApp,
     ) where
 
 import Network.Wai
@@ -13,6 +13,8 @@ import Control.Monad.Trans.Either (EitherT, runEitherT)
 import Data.Either.Unwrap (fromRight)
 import Database.SQLite.Simple
 import Control.Monad.IO.Class (liftIO)
+import Data.List (sortBy)
+import Data.Function (on)
 import Servant
 import Model
 
@@ -35,20 +37,61 @@ api = Proxy
 server :: Server API
 server = queryBridges :<|> queryStations
 
-bridge :: [Bridge]
-bridge = [Bridge 1 "" "" "" "" 2.0 "" "" 2.0 2.0]
-
-station :: [Station]
-station = [Station 1 "" 2.0 2.0 "" "" "" "" "" "" ""]
-
 queryBridges :: Maybe Double -> Maybe Double -> EitherT ServantErr IO [Bridge]
-queryBridges lat lng = do
-    liftIO $ print lat
-    liftIO $ print lng
-    conn <- liftIO (open "njdot-fuelup.db")
-    liftIO (query_ conn "SELECT * from bridges" :: IO [Bridge])
+queryBridges lat lng = 
+    case lat of
+        Nothing -> return []
+        Just la -> case lng of 
+                      Nothing -> return []
+                      Just ln -> do
+                          conn <- liftIO $ open "njdot-fuelup.db"
+                          query <- liftIO $ query_ conn "SELECT * from bridges"
+                          let sorted = getSorted $ bridgeDistances (la, ln) query
+                          liftIO $ close conn
+                          return sorted
 
 queryStations :: Maybe Double -> Maybe Double -> EitherT ServantErr IO [Station]
-queryStations lat lng = do
-    conn <- liftIO (open "njdot-fuelup.db")
-    liftIO (query_ conn "SELECT * from stations" :: IO [Station])
+queryStations lat lng =
+    case lat of
+        Nothing -> return []
+        Just la -> case lng of 
+                      Nothing -> return []
+                      Just ln -> do
+                          conn <- liftIO $ open "njdot-fuelup.db"
+                          query <- liftIO $ query_ conn "SELECT * from stations"
+                          let sorted = getSorted $ stationDistances (la, ln) query
+                          liftIO $ close conn
+                          return query
+
+-- Calculate Haversine
+
+haversine :: (Double, Double) -> (Double, Double) -> Double
+haversine (lat1,lng1) (lat2,lng2) =  earthRadius * c
+    where earthRadius = 6371
+          radians n = n * pi / 180
+          dLng = radians $ lng2 - lng1
+          dLat = radians $ lat2 - lat1
+          a = (sin (dLat/2))^2 + cos (radians lat1) * cos (radians lat2) * (sin (dLng/2))^2
+          c = 2 * (atan2 (sqrt a) (sqrt (1-a)))
+
+bridgeDistances :: (Double, Double) -> [Bridge] -> [(Double, Bridge)]
+bridgeDistances coord bridges = zipWith (,) (haversine coord <$> coords) bridges
+    where coords = (\x -> (bridgeLatitude x, bridgeLongitude x)) <$> bridges
+
+stationDistances :: (Double, Double) -> [Station] -> [(Double, Station)]
+stationDistances coord stations = zipWith (,) (haversine coord <$> coords) stations
+    where coords = (\x -> (stationLatitude x, stationLongitude x)) <$> stations
+
+getSorted :: [(Double,a)] -> [a]
+getSorted list = snd <$> sorted
+    where sorted = sortBy (compare `on` fst) list
+
+
+
+
+
+
+
+
+
+
--- src/Database.hs
+++ src/Database.hs
@@ -21,25 +21,18 @@ parseStations = do
 csvToStations :: IO CSVData -> IO [Station]
 csvToStations input = do
     csvList <- input
-    return . extractStations $ csvToStation <$> csvList
+    return . extractValues $ csvToStation <$> csvList
 
 csvToStation :: [String] -> Maybe Station
 csvToStation (a:b:c:d:e:f:g:h:i:j:k:[]) = 
     Just $ Station (read a :: Int) b (read c :: Double) (read d :: Double) e f g h i j k
 csvToStation _ = Nothing
 
-extractStations :: [Maybe Station] -> [Station]
-extractStations [] = []
-extractStations (x:xs) = case x of
-                          Just m -> m : extractStations xs
-                          _      -> extractStations xs
-
-persistStations :: IO ()
-persistStations = do
-    stations <- csvToStations parseStations
-    conn <- open "njdot-fuelup.db"
-    sequence_ $ execute conn "INSERT INTO stations VALUES (?,?,?,?,?,?,?,?,?,?,?)" <$> stations
-    close conn
+extractValues :: [Maybe a] -> [a]
+extractValues [] = []
+extractValues (x:xs) = case x of
+                          Just m -> m : extractValues xs
+                          _      -> extractValues xs
 
 -- BRIDGES
 
@@ -52,22 +45,45 @@ parseBridges = do
 csvToBridges :: IO CSVData -> IO [Bridge]
 csvToBridges input = do
     csvList <- input
-    return . extractBridges $ csvToBridge <$> csvList
+    return . extractValues $ csvToBridge <$> csvList
 
 csvToBridge :: [String] -> Maybe Bridge
 csvToBridge (a:b:c:d:e:f:g:h:i:j:[]) = 
-    Just $ Bridge (read a :: Int) b c d e (read f :: Double) g h (read i :: Double) (read j :: Double)
+    Just $ Bridge (read a :: Int) b c d e (read f :: Double) g h (read i :: Double) $ negate (read j :: Double)
 csvToBridge _ = Nothing
 
-extractBridges :: [Maybe Bridge] -> [Bridge]
-extractBridges [] = []
-extractBridges (x:xs) = case x of
-                          Just m -> m : extractBridges xs
-                          _      -> extractBridges xs
+-- DATABASE
 
-persistBridges :: IO ()
-persistBridges = do
-    bridges <- csvToBridges parseBridges
+initializeDB :: IO ()
+initializeDB = do
     conn <- open "njdot-fuelup.db"
+    execute_ conn "CREATE TABLE IF NOT EXISTS bridges (       \n\
+                  \    bridgeId           INTEGER PRIMARY KEY,\n\
+                  \    bridgeOwner        TEXT,               \n\
+                  \    bridgeRoute        TEXT,               \n\
+                  \    bridgeNumber       TEXT,               \n\
+                  \    bridgeName         TEXT,               \n\
+                  \    bridgeMp           REAL,               \n\
+                  \    bridgeCounty       TEXT,               \n\
+                  \    bridgeMunicipality TEXT,               \n\
+                  \    bridgeLatitude     REAL,               \n\
+                  \    bridgeLongitude    REAL                \n\
+                  \)"
+    execute_ conn "CREATE TABLE IF NOT EXISTS stations (      \n\
+                  \    stationId          INTEGER PRIMARY KEY,\n\
+                  \    stationName        TEXT,               \n\
+                  \    stationLatitude    REAL,               \n\
+                  \    stationLongitude   REAL,               \n\
+                  \    stationAddress     TEXT,               \n\
+                  \    stationCity        TEXT,               \n\
+                  \    stationState       TEXT,               \n\
+                  \    stationCounty      TEXT,               \n\
+                  \    stationHours       TEXT,               \n\
+                  \    stationPhoneNumber TEXT,               \n\
+                  \    stationTypeGas     TEXT                \n\
+                  \)"
+    stations <- csvToStations parseStations
+    bridges <- csvToBridges parseBridges
+    sequence_ $ execute conn "INSERT INTO stations VALUES (?,?,?,?,?,?,?,?,?,?,?)" <$> stations
     sequence_ $ execute conn "INSERT INTO bridges VALUES (?,?,?,?,?,?,?,?,?,?)" <$> bridges
     close conn
